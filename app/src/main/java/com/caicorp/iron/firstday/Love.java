9c934a6ceae5364954b645671e91a9ea1bfb56ca
Taesan <gbgo77@naver.com>
2018-10-03 18:32:29 +0900

    make love_model

---
+++ app/src/main/java/com/caicorp/iron/firstday/Love.java
@@ -0,0 +1,43 @@
+package com.caicorp.iron.firstday;
+
+public class Love {
+
+    int id       ;
+    String beginDate     ;
+    byte[] image1   ;
+    byte[] image2   ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getbeginDate() {
+        return beginDate;
+    }
+
+    public void setbeginDate(String beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public byte[] getImage1() {
+        return image1;
+    }
+
+    public void setImage1(byte[] image1) {
+        this.image1 = image1;
+    }
+
+    public byte[] getImage2() {
+        return image2;
+    }
+
+    public void setImage2(byte[] image2) {
+        this.image2 = image2;
+    }
+
+
+}
